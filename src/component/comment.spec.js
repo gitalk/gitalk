e5e5a6c13c53ec099611931b52e352297f752029
Liucw <liucw.mail@gmail.com>
2020-02-16 20:13:41 +0800

    test: email comments test case

--- src/component/comment.spec.js
+++ src/component/comment.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import Comment from './comment'
 import Avatar from './avatar'
@@ -7,6 +7,17 @@ import Avatar from './avatar'
 const comment = {
   html_url: 'https://github.com/xxxx/xxxxx/issues/1#issuecomment-xxxxx',
   body_html: '<p>123</p>',
+  created_at: '2017-06-30T09:01:19Z',
+  user: {
+    login: 'booxood',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/2151410?v=3',
+    html_url: 'https://github.com/booxood'
+  }
+}
+
+const emailComment = {
+  html_url: 'https://github.com/xxxx/xxxxx/issues/1#issuecomment-xxxxx',
+  body_html: '<div class="email-fragment">email response</div>↵<span class="email-hidden-toggle"><a href="#">…</a></span><div class="email-hidden-reply">↵<div class="email-signature-reply">------------------&amp;nbsp;原始邮件&amp;nbsp;------------------↵发件人:&amp;nbsp;↵↵↵↵↵xxxxxxxxxxxxxx↵↵—↵You are receiving this because you commented.↵Reply to this email directly, view it on GitHub, or unsubscribe.</div>↵</div>',
   created_at: '2017-06-30T09:01:19Z',
   user: {
     login: 'booxood',
@@ -123,5 +134,20 @@ describe('Comment', function () {
 
     const wrapper = shallow(<Comment {...props} />)
     expect(wrapper.render().find('.gt-ico-heart .gt-ico-text').text()).toEqual('100+')
+  })
+
+  it('render extended emailed comment and add event listener', function () {
+    const props = {
+      comment: emailComment
+    }
+    const wrapper = mount(<Comment {...props} />)
+    expect(wrapper.render().find('.gt-comment-body > .email-fragment')).toHaveLength(1)
+    expect(wrapper.render().find('.gt-comment-body > .email-hidden-toggle')).toHaveLength(1)
+    expect(wrapper.render().find('.gt-comment-body > .email-hidden-reply')).toHaveLength(1)
+
+    // TODO: test click
+    // enzyme don't support simulate click after render()
+    // wrapper.render().find('.email-hidden-toggle > a').simulate('click')
+    // expect(wrapper.render().find('.gt-comment-body > .email-hidden-reply.expanded')).toHaveLength(1)
   })
 })
