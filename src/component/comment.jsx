e772a55d9869374a51e2fd132ea71730152cce03
MH <616001163@qq.com>
2020-02-20 06:51:52 +0800

    Update comment.jsx

--- src/component/comment.jsx
+++ src/component/comment.jsx
@@ -44,7 +44,7 @@ export default class Comment extends Component {
   }
 
   handleImageErrored(obj) {
-	obj.target.src="https://cdn.jsdelivr.net/npm/gitalk@1/src/assets/icon/github.svg";
+    obj.target.src="https://cdn.jsdelivr.net/npm/gitalk@1/src/assets/icon/github.svg";
   }
   render () {
     const {
@@ -74,13 +74,14 @@ export default class Comment extends Component {
         reactionTotalCount = '100+'
       }
     }
+
     return (
       <div ref={node => { this.node = node }} className={`gt-comment ${isAdmin ? 'gt-comment-admin' : ''}`}>
         <Avatar
           className="gt-comment-avatar"
           src={comment.user && comment.user.avatar_url}
           alt={comment.user && comment.user.login}
-		  onError={this.handleImageErrored.bind(this)} 
+          onError={this.handleImageErrored.bind(this)} 
         />
 
         <div className="gt-comment-content">
